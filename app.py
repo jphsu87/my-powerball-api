2916dcaa69eef7399ac50d61536491c1f1a32707
Jason Hsu <jphsu87@gmail.com>
2025-08-04 14:37:18 -0700

    Updated app.py to fix history issue

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def get_history():
             "powerball": int(row['powerball'])
         })
 
-    return jsonify(results)
+    return jsonify(draws=results)
 
 # Optional: Reload CSV data manually
 @app.route('/reload', methods=['POST'])
